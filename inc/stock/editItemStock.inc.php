4aef07891467462a736e8bc527d5b1dbd2d5b5c9
ym <ymairesse@sio2.be>
2024-01-10 20:11:03 +0100

    Correction pour l'échappement des quotes

--- inc/stock/editItemStock.inc.php
+++ inc/stock/editItemStock.inc.php
@@ -12,6 +12,10 @@ $idMateriel = isset($_POST['idMateriel']) ? $_POST['idMateriel'] : Null;
 
 $itemStock = $Stock->getOneItemStock($idMateriel);
 
+$itemStock['marque'] = isset($itemStock['marque']) ? htmlspecialchars($itemStock['marque'], ENT_QUOTES, 'UTF-8') : Null;
+$itemStock['modele'] = isset($itemStock['modele']) ? htmlspecialchars($itemStock['modele'], ENT_QUOTES, 'UTF-8') : Null;
+$itemStock['caracteristiques'] = isset($itemStock['caracteristiques']) ? htmlspecialchars($itemStock['caracteristiques'], ENT_QUOTES, 'UTF-8') : Null;
+
 $smarty->assign('idMateriel', $idMateriel);
 $smarty->assign('itemStock', $itemStock);
 
