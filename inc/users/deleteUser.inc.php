3a18aa1168a5e5246ab891fcd5b4fc88815612d5
yves <yves@oxfaminfo.be>
2024-05-09 18:16:35 +0200

    Ajout d'un commentaire

--- inc/users/deleteUser.inc.php
+++ inc/users/deleteUser.inc.php
@@ -7,6 +7,7 @@ require_once '../../config.inc.php';
 // ressources principales toujours nécessaires: classes Application, User, Smarty,
 include '../entetes.inc.php';
 
+// identifiant de l'utilisateur à supprimer
 $idUser = isset($_POST['idUser']) ? $_POST['idUser'] : null;
 
 $n = $User->delUser($idUser);
